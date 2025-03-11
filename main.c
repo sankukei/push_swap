a25718bd364fc25c55f24bac748c0a8ebac9c967
Leo Thomas <leothoma@paul-f4Ar2s4.clusters.42paris.fr>
2025-03-11 23:56:43 +0100

    xd

--- main.c
+++ main.c
@@ -81,17 +81,88 @@ char	**ft_parse(char *str)
 	return (res = ft_split(str, ' '));
 }
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	no_doubles(char *str)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+	y = 0;
+	while (str[i])
+	{
+		y = 0;
+		while (str[y])
+		{
+			y++;
+			if (str[i] == str[y])
+				return (1);
+		}
+ 		i++;
+	}
+	return (0);
+}
+
+int	check_parsing(char **av)
+{
+	int	i;
+	int	y;
+	int	j;
+	int	count;
+	char	buffer[10000];
+
+	i = 1;
+	y = 0;
+	j = 0;
+	while (av[i])
+	{
+		while (av[i][y])
+		{
+			if (!is_digit(av[i][y]))
+				return (1);
+			buffer[j] = av[i][y];
+			y++;
+			j++;
+		}
+		i++;
+		y = 0;
+	}
+	count = no_doubles(buffer);
+	if (count == 1)
+		return (1);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {	
 	t_list *stack_a;
 	t_list *stack_b;
 	char	**args;
+	int	check;
 
+	check = 0;
 	stack_a = 0;
 	stack_b = 0;
+	check = check_parsing(av);
 	if (ac == 1)
 		return (0);
-	if (ac > 1)
+	if (ac > 1 && check == 0)
 	{
 		if (ac == 2)
 		{
